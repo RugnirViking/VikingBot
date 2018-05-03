435ede91ce126a10e094007710e364eb94d1f587
Samuel Howell <rugnirtheviking@gmail.com>
2018-05-03 19:36:57 +0200

    Small change

--- main.js
+++ main.js
@@ -11,12 +11,7 @@ socket.on('disconnect', function() {
 socket.on('connect', function() {
 	console.log('Connected to server.');
 
-	/* Don't lose this user_id or let other people see it!
-	 * Anyone with your user_id can play on your bot's account and pretend to be your bot.
-	 * If you plan on open sourcing your bot's code (which we strongly support), we recommend
-	 * replacing this line with something that instead supplies the user_id via an environment variable, e.g.
-	 * var user_id = process.env.BOT_USER_ID;
-	 */
+	
 	var user_id = 'VikingBot';
 	var username = 'VikingBot';
 	console.log(username);
@@ -24,28 +19,12 @@ socket.on('connect', function() {
 	socket.emit('set_username', user_id, username);
 
 	// Join a custom game and force start immediately.
-	// Custom games are a great way to test your bot while you develop it because you can play against your bot!
 	var custom_game_id = 'vikingBotPlayground';
 	socket.emit('join_private', custom_game_id, user_id);
 	socket.emit('set_force_start', custom_game_id, true);
 	console.log('Joined custom game at http://bot.generals.io/games/' + encodeURIComponent(custom_game_id));
-
-	// When you're ready, you can have your bot join other game modes.
-	// Here are some examples of how you'd do that:
-
-	// Join the 1v1 queue.
-	// socket.emit('join_1v1', user_id);
-
-	// Join the FFA queue.
-	// socket.emit('play', user_id);
-
-	// Join a 2v2 team.
-	// socket.emit('join_team', 'team_name', user_id);
 });
 
-// Terrain Constants.
-// Any tile with a nonnegative value is owned by the player corresponding to its value.
-// For example, a tile with value 1 is owned by the player with playerIndex = 1.
 var TILE_EMPTY = -1;
 var TILE_MOUNTAIN = -2;
 var TILE_FOG = -3;
@@ -148,6 +127,8 @@ socket.on('game_update', function(data) {
 			6 - if we are well and truly stuck, do nothing 
 	*/
 	// first of all, can we see a general?
+
+	// to be implemented later
 
 	// can we see an enemy tile?
 	var canSeeEnemy = false;
